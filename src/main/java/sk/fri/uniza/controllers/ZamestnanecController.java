f31d39fc5312b427f5c0b86e00279bdaaa21a2c2
richard <richardkovacik.123@gmail.com>
2021-11-02 08:48:31 +0100

    pridanie update funkcionality

--- src/main/java/sk/fri/uniza/controllers/ZamestnanecController.java
+++ src/main/java/sk/fri/uniza/controllers/ZamestnanecController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import sk.fri.uniza.exceptions.ZamestnanecNotFoundExcption;
 import sk.fri.uniza.model.Zamestnanec;
 import sk.fri.uniza.service.ZamestnanecService;
 
@@ -23,13 +25,28 @@ public class ZamestnanecController {
     @GetMapping(value = "/zamestnanci/novy")
     public String vratFormular(Model model) {
         model.addAttribute("zam", new Zamestnanec());
+        model.addAttribute("title", "Pridavanie noveho zamestnanca");
         return "zamestnanec_form";
     }
 
     @PostMapping("/zamestnanci/uloz")
     public String ulozZamestnanca(Zamestnanec zamestnanec, RedirectAttributes ra) {
         service.uloz(zamestnanec);
-        ra.addFlashAttribute("message","Zamestanaec bol uspesne pridany!");
+        ra.addFlashAttribute("message", "Zmeny boli uspesne ulozene!");
+        return "redirect:/zamestnanci";
+    }
+
+    @GetMapping("/zamestnanci/edit/{id}")
+    public String showEditForm(@PathVariable("id") int id, Model model, RedirectAttributes ra) {
+        try {
+            Zamestnanec zamestnanec = service.getZamestnanec(id);
+            service.uloz(zamestnanec);
+            model.addAttribute("zam", zamestnanec);
+            model.addAttribute("title", "Edit zamestnanca s id:" + id);
+            return "zamestnanec_form";
+        } catch (ZamestnanecNotFoundExcption e) {
+            ra.addFlashAttribute("exp", e.getMessage());
+        }
         return "redirect:/zamestnanci";
     }
 }
